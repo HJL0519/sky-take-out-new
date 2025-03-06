39df4a8c82ac330e65e23900bba3c5ea96be3249
HJL0519 <1412016345@qq.com>
2025-03-06 22:07:37 +0800

    day2-根据id查询员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -105,4 +105,12 @@ public class EmployeeController {
         return Result.success();
     }
 
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工")
+    public Result<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工,{}",id);
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
 }
