222b5b3caa04d0c0ef785de4ca7d38e3d8397273
HJL0519 <1412016345@qq.com>
2025-03-07 10:47:52 +0800

    day2-分类相关接口

--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.sky.service.impl;
 
 import com.sky.service.CategoryService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 }
