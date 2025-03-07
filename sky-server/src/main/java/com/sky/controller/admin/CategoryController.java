b9e67c86a860feb1bfae9d6f1db348c84ebf8049
HJL0519 <1412016345@qq.com>
2025-03-07 13:32:27 +0800

    day2-分类管理相关接口实现

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,16 +1,18 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -28,6 +30,50 @@ public class CategoryController {
     public Result save(@RequestBody CategoryDTO categoryDTO){
         log.info("新增分类,{}",categoryDTO);
         categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+    //分页展示
+    @GetMapping("/page")
+    @ApiOperation("分页展示")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+        log.info("分页展示,{}",categoryPageQueryDTO);
+        PageResult pageResult = categoryService.page(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    //启用禁用菜品分类
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用菜品分类")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用菜品分类,{},{}",status,id);
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    //根据类型查询分类
+    @GetMapping("/list")
+    @ApiOperation("根据类型查询分类")
+    public Result<List<Category>> list(Integer type){
+        List<Category> list = categoryService.list(type);
+        return Result.success(list);
+    }
+
+    //修改分类
+    @PutMapping
+    @ApiOperation("修改分类")
+    public Result updateCategory(@RequestBody CategoryDTO categoryDTO){
+        log.info("修改分类,{}",categoryDTO);
+        categoryService.updateCategory(categoryDTO);
+        return Result.success();
+    }
+
+    //根据id删除分类
+    @DeleteMapping
+    @ApiOperation("根据id删除分类")
+    public Result deleteCategory(Long id){
+        log.info("根据id删除分类,{}",id);
+        categoryService.deleteCategoty(id);
         return Result.success();
     }
 }
