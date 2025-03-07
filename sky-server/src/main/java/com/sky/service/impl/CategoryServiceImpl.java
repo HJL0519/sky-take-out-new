f9b8ad7517ada9b1e1a205bb0a26f59798c70eff
HJL0519 <1412016345@qq.com>
2025-03-07 11:20:06 +0800

    day2-新增分类

--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,45 @@
 package com.sky.service.impl;
 
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
+import com.sky.entity.Category;
+import com.sky.mapper.CategoryMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
 import com.sky.service.CategoryService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+
+    @Override
+    public void save(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO,category);
+
+        //初始状态设置为禁用
+        category.setStatus(StatusConstant.DISABLE);
+
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateUser(BaseContext.getCurrentId());
+
+        categoryMapper.insert(category);
+    }
 }
