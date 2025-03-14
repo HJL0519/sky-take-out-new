2e8588f8956258b93b992abf1024e94152e20204
HJL0519 <1412016345@qq.com>
2025-03-14 15:46:13 +0800

    day8-用户下单等相关接口实现

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,14 @@
+package com.sky.controller.user;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Api("C端订单相关接口")
+@Slf4j
+public class OrderController {
+
+}
