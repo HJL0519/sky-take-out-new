f695ff724e59564811990ef8e445fdec5d831122
HJL0519 <1412016345@qq.com>
2025-03-16 19:48:41 +0800

    day11-Top10商品统计相关接口实现

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -51,5 +52,14 @@ public class ReportController {
         log.info("订单统计,{},{}",begin,end);
         OrderReportVO orderReportVO = reportService.getOrderStatistics(begin,end);
         return Result.success(orderReportVO);
+    }
+
+    @GetMapping("/top10")
+    @ApiOperation("销量前10")
+    public Result<SalesTop10ReportVO> getTop10(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+                                               @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("销量前10,{},{}",begin,end);
+        SalesTop10ReportVO salesTop10ReportVO = reportService.getTop10(begin,end);
+        return Result.success(salesTop10ReportVO);
     }
 }
