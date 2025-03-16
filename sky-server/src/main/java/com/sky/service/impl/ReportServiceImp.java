8cb0bcfb62300e54b40cb75499b414e911b1c5e1
HJL0519 <1412016345@qq.com>
2025-03-16 16:10:05 +0800

    day11-订单统计相关接口实现

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImp.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImp.java
@@ -4,6 +4,7 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +15,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @Slf4j
@@ -107,6 +105,61 @@ public class ReportServiceImp implements ReportService {
                 .dateList(StringUtils.join(dateList,","))
                 .newUserList(StringUtils.join(newUserList,","))
                 .totalUserList(StringUtils.join(totalUserList,","))
+                .build();
+    }
+
+    @Override
+    public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end) {
+
+        //计算并存储日期数据
+        List<LocalDate> localDateList = new ArrayList<>();
+        localDateList.add(begin);
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);
+            localDateList.add(begin);
+        }
+
+        //每日订单数，以逗号分隔，例如：260,210,215
+        List<Integer> orderCountList = new ArrayList<>();
+        //每日有效订单数(状态为已完成)，以逗号分隔，例如：20,21,10
+        List<Integer> vaildOrderCountList = new ArrayList<>();
+
+        for (LocalDate date : localDateList) {
+            //获取date当天的开始时间(时分秒)和结束时间(时分秒)
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map todayOrderMap = new HashMap<>();
+            todayOrderMap.put("begin",beginTime);
+            todayOrderMap.put("end",endTime);
+            Integer todayOrder = orderMapper.countByMap(todayOrderMap);
+            todayOrder = (todayOrder == null ? 0 : todayOrder);
+            todayOrderMap.put("status",Orders.COMPLETED);
+            Integer todayVaildOrder = orderMapper.countByMap(todayOrderMap);
+            todayVaildOrder = (todayVaildOrder == null ? 0 : todayVaildOrder);
+
+            orderCountList.add(todayOrder);
+            vaildOrderCountList.add(todayVaildOrder);
+        }
+
+        //计算时间区间内的订单总数和有效订单数
+        Integer totalOrderCount = orderCountList.stream().reduce(Integer::sum).get();
+        Integer vaildOrderCount = vaildOrderCountList.stream().reduce(Integer::sum).get();
+
+        //计算订单完成率
+        Double orderCompletionRate = 0.0;
+        if (totalOrderCount != 0){
+            orderCompletionRate = vaildOrderCount.doubleValue() / totalOrderCount;
+        }
+
+        return OrderReportVO
+                .builder()
+                .dateList(StringUtils.join(localDateList,","))
+                .orderCountList(StringUtils.join(orderCountList,","))
+                .validOrderCountList(StringUtils.join(vaildOrderCountList,","))
+                .totalOrderCount(totalOrderCount)
+                .validOrderCount(vaildOrderCount)
+                .orderCompletionRate(orderCompletionRate)
                 .build();
     }
 }
