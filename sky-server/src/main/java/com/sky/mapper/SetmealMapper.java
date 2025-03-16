cc4de225c946e36c9c037282c6390fa828cb3f1f
HJL0519 <1412016345@qq.com>
2025-03-16 20:58:11 +0800

    day12-工作台相关接口实现

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -37,4 +38,6 @@ public interface SetmealMapper {
     List<Setmeal> getList(Setmeal setmeal);
 
     List<DishItemVO> getDishList(Long id);
+
+    Integer countByMap(Map map);
 }
