59a9bc369e9870a752bb76d8130223ea74c4011c
HJL0519 <1412016345@qq.com>
2025-03-16 22:09:22 +0800

    day12-运营报表的填充与下载相关接口实现

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -61,5 +63,11 @@ public class ReportController {
         log.info("销量前10,{},{}",begin,end);
         SalesTop10ReportVO salesTop10ReportVO = reportService.getTop10(begin,end);
         return Result.success(salesTop10ReportVO);
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 }
